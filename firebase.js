f5264f9539b1447c2b523b07667034fb98b47637
OliviaDelisa <oliviadelisa123@gmail.com>
2025-12-25 18:16:07 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,12 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./notif-ad71d-firebase-adminsdk-fbsvc-54659b2818.json");
+
+// Ambil credential dari Environment Variable Render
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
 if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-    });
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 }
 
 module.exports = admin;
